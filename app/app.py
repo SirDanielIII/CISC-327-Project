4d7b4c5454389c6a56376a96c243eb0406d17099
Ayman Zaher <11262003a@gmail.com>
2024-10-19 19:10:06 -0400

    Added Flask-Login to app and user model

--- app/app.py
+++ app/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_login import LoginManager
 from controllers import public_controller, account_controller
 
 app = Flask(__name__)
 app.secret_key = '743e38e4152d2160384c9027fb6b8b85'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = account_controller.login
 
 app.register_blueprint(public_controller.public_blueprint)
 app.register_blueprint(account_controller.account_blueprint)
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,7 +1,10 @@
-class User():
-    
+from flask_login import UserMixin
+
+class User(UserMixin):
+    id: int
     email: str
     password: str
+    token_2fa: str | None = None
 
     def __init__(self, email, password) -> None:
         self.email = email
