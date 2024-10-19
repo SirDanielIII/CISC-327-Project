51563fe990f5053eb63394b07cd4aa36656c9377
Ayman Zaher <11262003a@gmail.com>
2024-10-19 12:06:52 -0400

    Added basic message flashing using Flask flash.
    This still needs styling work

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from controllers import public_controller, account_controller
 
 app = Flask(__name__)
+app.secret_key = '743e38e4152d2160384c9027fb6b8b85'
 
 app.register_blueprint(public_controller.public_blueprint)
 app.register_blueprint(account_controller.account_blueprint)
