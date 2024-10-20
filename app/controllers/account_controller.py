9ccdaa651610cc19574c1b5a20fa1ef20ca6b700
Ayman Zaher <11262003a@gmail.com>
2024-10-20 14:10:41 -0400

    Basic account controller still needing working

--- app/controllers/account_controller.py
+++ app/controllers/account_controller.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, redirect, request, flash
+from flask_login import login_user, login_required
 from models import in_memory_users
+from models.user_model import User, UserRoles
 
 account_blueprint = Blueprint('account', __name__)
 
@@ -12,19 +14,39 @@ def login():
         for user in in_memory_users:
             if user.email == email and user.password == password:
                 # If success, redirect to next page
+                login_user(user)
                 return redirect('/') # TODO
         # If failure, flash an error message and render_template
         flash('The email or password provided is invalid! Please verify it has been entered correctly.', 'error')
     return render_template('account/login.html')
 
-@account_blueprint.route('/register', methods=['GET'])
+@account_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # TODO: Backend implementation
+        # For now always success
+        new_user = User(email, password, UserRoles.TENANT)
+        in_memory_users.append(new_user)
+        login_user(new_user)
+        return redirect('/setup_2fa')
     return render_template('account/register.html')
 
-@account_blueprint.route('/setup_2fa', methods=['GET'])
+@account_blueprint.route('/setup_2fa', methods=['GET', 'POST'])
+@login_required
 def setup_2fa():
+    if request.method == 'POST':
+        # TODO: Backend implementation
+        # For now always success
+        return redirect('/') # TODO
     return render_template('account/setup_2fa.html')
 
-@account_blueprint.route('/verify_2fa', methods=['GET'])
+@account_blueprint.route('/verify_2fa', methods=['GET', 'POST'])
+@login_required
 def verify_2fa():
+    if request.method == 'POST':
+        # TODO: Backend implementation
+        # For now always success
+        return redirect('/') # TODO
     return render_template('account/verify_2fa.html')
