1fbe39659e04881638f4b3f02dbd5d25b57c45a6
Ayman Zaher <11262003a@gmail.com>
2024-10-20 12:37:21 -0400

    Added user loader for flask login and some other fixes

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,5 +1,7 @@
 from flask_login import UserMixin
 
+next_user_id = 1
+
 class User(UserMixin):
     id: int
     email: str
@@ -7,5 +9,7 @@ class User(UserMixin):
     token_2fa: str | None = None
 
     def __init__(self, email, password) -> None:
+        self.id = next_user_id
+        next_user_id = next_user_id + 1
         self.email = email
         self.password = password
