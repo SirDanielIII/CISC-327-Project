43a09c7307cacc06ced50f1c5e6a08c040fe46f0
Ayman Zaher <11262003a@gmail.com>
2024-10-20 13:49:05 -0400

    Added user roles to the User model
    Roles are TENANT, PROPETY_OWNER or ADMINISTRATOR.
    
    Also renamed reigstar.html to register.html

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,4 +1,10 @@
 from flask_login import UserMixin
+from enum import Enum
+
+class UserRoles(Enum):
+    TENANT = 1
+    PROPETY_OWNER = 2
+    ADMINISTRATOR = 3
 
 next_user_id = 1
 
@@ -7,9 +13,11 @@ class User(UserMixin):
     email: str
     password: str
     token_2fa: str | None = None
+    role: UserRoles
 
-    def __init__(self, email, password) -> None:
+    def __init__(self, email, password, role) -> None:
         self.id = next_user_id
         next_user_id = next_user_id + 1
         self.email = email
         self.password = password
+        self.role = role
