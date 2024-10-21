1ef689048f1b82ec683c648c6e2ef27998f5b305
Ayman Zaher <11262003a@gmail.com>
2024-10-21 14:03:59 -0400

    Implemented the property controller
    Also added Id's to the property model

--- app/models/property_model.py
+++ app/models/property_model.py
@@ -1,3 +1,7 @@
 class Property():
+    next_property_id = 1
+
     def __init__(self, owner_id) -> None:
+        self.id = Property.next_property_id
+        Property.next_property_id+=1
         self.owner_id = owner_id
