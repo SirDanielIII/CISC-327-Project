d2d3f3617482dc94c8feff74b60aec694d4d4198
Ayman Zaher <11262003a@gmail.com>
2024-10-26 23:50:41 -0400

    Added first and last name to user model

--- app/enums/DatabaseType.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class DatabaseType(Enum):
-    USER = 'user'
-    PROPERTY = 'property'
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -5,6 +5,8 @@ from ..enums.AccountType import AccountType
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(40), unique=True, nullable=False)
+    first_name = db.Column(db.String(40), nullable=True)
+    last_name = db.Column(db.String(40), nullable=True)
     password = db.Column(db.String(1024), nullable=False)
     account_type = db.Column(db.Enum(AccountType), nullable=False)
     token_2fa = db.Column(db.String(1024), nullable=True)
