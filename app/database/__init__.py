6889cc5aa389c640e701d4bbddd46ba5af08254f
Ayman Zaher <11262003a@gmail.com>
2024-10-27 00:13:33 -0400

    Fixed unit test using same db as prod

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, upgrade
+from flask_migrate import Migrate, upgrade, stamp
 
 DATABASE_NAME = 'rent_management.db'
+UNIT_TEST_DATABASE_NAME = f'unit_test_{DATABASE_NAME}'
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,7 +12,7 @@ migrate = Migrate()
 def initialize_db(app: Flask, unit_test=False):
     db_directory = os.path.dirname(__file__)
 
-    db_path = os.path.join(db_directory, DATABASE_NAME)
+    db_path = os.path.join(db_directory, UNIT_TEST_DATABASE_NAME if unit_test else DATABASE_NAME)
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -26,6 +27,7 @@ def initialize_db(app: Flask, unit_test=False):
         if not os.path.exists(DATABASE_NAME):
             # db doesn't exist, creating it...
             db.create_all()
+            stamp()
 
         # Upgrade the db to the latest schema
         upgrade()
