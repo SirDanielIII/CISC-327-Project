01d81d49abe878a675d040edf2b308b514576ea7
Daniel Zhuo <sirdanieldathird@gmail.com>
2024-11-05 02:28:18 -0500

    Fixed test case for new Error 404 page

--- tests/test_public.py
+++ tests/test_public.py
@@ -11,8 +11,8 @@ class PublicTests(BaseTestClass):
         """Test the custom not found page is used"""
         response = self.client.get('/not-found')
         self.assertEqual(response.status_code, 404)
-        self.assertIn(b"We couldn't find the page you are looking for", response.data)
+        self.assertIn(b"Oops! We couldn't find the page you were looking for", response.data)
 
         response = self.client.get('/other-not-found')
         self.assertEqual(response.status_code, 404)
-        self.assertIn(b"We couldn't find the page you are looking for", response.data)
+        self.assertIn(b"Oops! We couldn't find the page you were looking for", response.data)
