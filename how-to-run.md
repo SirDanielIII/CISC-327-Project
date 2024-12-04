5e1b367c7800c1c713aff8f832982ed37e944acf
leezeleeze <ajlb6426@gmail.com>
2024-12-04 18:12:16 -0500

    error checked, but url_changes function have some issues despite working

--- how-to-run.md
+++ how-to-run.md
@@ -5,23 +5,16 @@
     ```powershell
     python --version
     ```
-
     or if you're on a Unix system:
-
     ```bash
     python3 --version
     ```
-
 2. Install the required packages by running the following command:
-
     ```bash
     pip install -r requirements.txt
     ```
-
 ## Running The Web Server
-
 Run the following command to start the server:
-
 ```powershell
 python run_app.py
 ```
@@ -34,6 +27,7 @@ python3 run_app.py
 
 ## Running The Test Suite
 
+### Running Unit and Integration Tests
 Run the following command to start run the unit tests with code coverage analysis:
 
 ```powershell
@@ -44,4 +38,13 @@ or if you're on a Unix system:
 
 ```bash
 python3 run_tests.py
+```
+### Running End-To-End Tests
+Run the following command to start run the end to end test suite:
+```powershell
+python run_e2e_tests.py
+```
+or if you're on a Unix system:
+```bash
+python3 run_e2e_tests.py
 ```
--- tests_e2e/base_test_class.py
+++ tests_e2e/base_test_class.py
@@ -15,47 +15,56 @@ from app.models import User
 
 WEB_SERVER_PORT = 8080
 
+
 class BaseTestClass(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         db_name_prefix = f'{uuid4()}_account_tests_'
         cls.app = create_app(db_name_prefix)
         cls.app.config['TESTING'] = True
+
         cls.user_first_name = 'Property'
         cls.user_last_name = 'Owner'
         cls.user_email = 'propertyowner@example.com'
         cls.user_password = 'Propertyowner1'
+
         cls.user_logged_out_welcome_msg = "Welcome to the Rental Management System"
         cls.user_logged_in_welcome_msg = f"Welcome {cls.user_first_name}, to the Rental Management System"
+
         with cls.app.app_context():
             cls.test_user = User(first_name=cls.user_first_name, last_name=cls.user_last_name, email=cls.user_email,
                                  password=cls.user_password, account_type=AccountType.PROPERTY_OWNER)
             db.session.add(cls.test_user)
             db.session.commit()
             db.session.refresh(cls.test_user)
+
         # Start the web app in a separate thread
         from werkzeug.serving import make_server
         cls.server = make_server('localhost', WEB_SERVER_PORT, cls.app)
         cls.server_thread = threading.Thread(target=cls.server.serve_forever)
         cls.server_thread.start()
+
         # Setting up selenium
         cls.driver = webdriver.Chrome(options=webdriver.ChromeOptions())
 
     @classmethod
     def tearDownClass(cls) -> None:
         cls.driver.quit()
+
         # Stop the web app
         cls.server.shutdown()
+
         with cls.app.app_context():
             db.session.remove()
             db.drop_all()
             db.engine.dispose()
             os.remove(db.db_path)
-   
+
     def setUp(self) -> None:
         # Reset driver state
         self.driver.delete_all_cookies()
         self.driver.get(f'http://localhost:{WEB_SERVER_PORT}')
+
         with self.app.app_context():
             # Disable any 2FA that may have been setup in a test case
             self.test_user = db.session.get(User, self.test_user.id)
@@ -64,7 +73,7 @@ class BaseTestClass(unittest.TestCase):
         self.client = self.app.test_client()
         self.test_user_2fa_token = None
         return super().setUp()
-    
+
     def enableTestUser2fa(self) -> Any | None:
         if self.test_user_2fa_token:
             return
@@ -75,9 +84,10 @@ class BaseTestClass(unittest.TestCase):
             db.session.commit()
             self.test_user_2fa_token = test_user.token_2fa
             return test_user.token_2fa
+
     def getFullWebPath(self, path: str) -> str:
         return f'http://localhost:{WEB_SERVER_PORT}{path}'
-    
+
     def pageFlashesContain(self, expectedStr: str) -> bool:
         flashesContainer = self.driver.find_element(By.ID, 'flashes')
         if not flashesContainer:
@@ -87,24 +97,30 @@ class BaseTestClass(unittest.TestCase):
             if expectedStr in flash.text:
                 return True
         return False
-    
+
     def loginTestUser(self):
         login_full_path = self.getFullWebPath('/login')
         original_url = self.driver.current_url
         self.driver.get(login_full_path)
+
         wait = WebDriverWait(self.driver, 10).until(
             EC.url_changes(original_url)
         )
+
         self.assertEqual(login_full_path, self.driver.current_url)
+
         login_button = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.ID, 'login-submit'))
+            EC.element_to_be_clickable((By.ID, 'login-submit'))
         )
+
         email_input = self.driver.find_element(By.NAME, 'email')
         email_input.send_keys(self.user_email)
+
         password_input = self.driver.find_element(By.NAME, 'password')
         password_input.send_keys(self.user_password)
+
         login_button.click()
+
         # Check that the user is logged in
         nav_fullname = self.driver.find_element(By.ID, 'nav-user-fullname')
         self.assertEqual(f'{self.user_first_name} {self.user_last_name}', nav_fullname.text)
-        self.assertTrue(self.pageFlashesContain("Logged in successfully!"))
